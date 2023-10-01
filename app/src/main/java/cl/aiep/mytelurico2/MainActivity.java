ccedcb2e71960e55f3ef37515e5b03b856e89fa3
buitrekommando <jl.henriquez@gmail.com>
2023-10-01 20:16:27 -0300

    Initial commit,aca el codigo esta tal cual lo dictó el porfesor en clase.

---
+++ app/src/main/java/cl/aiep/mytelurico2/MainActivity.java
@@ -0,0 +1,73 @@
+package cl.aiep.mytelurico2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultadoTextview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultadoTextview = findViewById(R.id.teluricoTextView);
+        new HttGetRequest().execute();
+    }
+     private class HttGetRequest extends AsyncTask<Void, Void, String>{
+        @Override
+        protected String doInBackground(Void... voids){
+            String apiURL = "https://api.gael.cloud/general/public/sismos";
+            try {
+                // Conexión
+                URL url = new URL(apiURL);
+                HttpsURLConnection connexion = (HttpsURLConnection) url.openConnection();
+                connexion.setRequestMethod("GET");
+
+                // Leer respuesta del server
+                InputStream inputStream = connexion.getInputStream();
+                BufferedReader reader = new BufferedReader((new InputStreamReader(inputStream)));
+                StringBuilder resultado = new StringBuilder();
+                String linea;
+                while ((linea = reader.readLine())!= null) {
+                    resultado.append(linea);
+
+                }
+
+                // Cerrar las conexiones
+                //Retrofit esa es la que hay que usar.
+                reader.close();
+                inputStream.close();
+                connexion.disconnect();
+
+                return resultado.toString();
+
+
+            }catch (IOException e){
+                e.printStackTrace();
+                return "Error al realizar la solicitud";
+            }
+
+        }
+
+        @Override
+         protected void onPostExecute(String resultado){
+            super.onPostExecute(resultado);
+            resultadoTextview.setText(resultado);
+
+        }
+     }
+
+}
